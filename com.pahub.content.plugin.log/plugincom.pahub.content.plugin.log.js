caeb132a42a840bcb6685e08417d077726ac286b
Raevn <d_bened@raevn.com>
2014-11-24 21:44:56 +1100

    Update to v0.1.1
    Change: Removed debug setting controls
    Change: Removed label text from section

--- com.pahub.content.plugin.log/plugincom.pahub.content.plugin.log.js
+++ com.pahub.content.plugin.log/plugincom.pahub.content.plugin.log.js
@@ -5,16 +5,16 @@ function load_plugin_log(data, folder) {
 		show_verbose_log: ko.observable(false)
 	}
 
-	pahub.api.section.addSection("section-log", "LOG", path.join(folder, "log.png"), "header", 10);
+	pahub.api.section.addSection("section-log", "", path.join(folder, "log.png"), "header", 10);
 	pahub.api.tab.addTab("section-log", "log", "", "", 10);
 	
 	pahub.api.resource.loadResource(path.join(folder, "log.html"), "get", {name: "HTML: log", mode: "async", success: function(resource) {
 		pahub.api.tab.setTabContent("section-log", "log", resource.data);
 	}});
 	
-	pahub.api.setting.addSettingGroup("log", "Log Settings [Debug]");
-	pahub.api.setting.addSetting("log", "plugin.log.show_verbose_log", model.log.show_verbose_log, "boolean", "checkbox", false, "show_verbose_log", null, {});
-	pahub.api.setting.addSetting("log", "plugin.log.show_debug_log", model.log.show_debug_log, "boolean", "checkbox", false, "show_debug_log", null, {});
+	//pahub.api.setting.addSettingGroup("log", "Log Settings [Debug]");
+	pahub.api.setting.addSetting("log", "plugin.log.show_verbose_log", model.log.show_verbose_log, "boolean", null, false, "show_verbose_log", null, {});
+	pahub.api.setting.addSetting("log", "plugin.log.show_debug_log", model.log.show_debug_log, "boolean", null, false, "show_debug_log", null, {});
 }
 
 function unload_plugin_log(data) {
